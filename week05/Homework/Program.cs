105ab1681b0b9ab41cc7e4e15a35afa655f66995
TysonJay00 <twilde@byupathway.edu>
2026-02-10 21:10:32 -0700

    initial commit

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment math1 = new MathAssignment("Deimian Wilde", "Multiplication", "7.3", "8 - 19");
+
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment writing1 = new WritingAssignment("Kary Ramirez", "Mexican History", "The Spanish Invasion");
+        
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
     }
+
+   
 }
